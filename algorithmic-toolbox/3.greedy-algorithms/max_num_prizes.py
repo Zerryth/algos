d00d679ecc11dce692a1a340f08559e41799a6dd
Ashley Finafrock <v-asho@microsoft.com>
2021-03-17 22:06:18 -0700

    Completed max number of prizes

--- algorithmic-toolbox/3.greedy-algorithms/max_num_prizes.py
+++ algorithmic-toolbox/3.greedy-algorithms/max_num_prizes.py
@@ -9,7 +9,7 @@ You have n pieces of candy (positive integer), and want to divvy the candy up am
 
 What is the maximum number of winners (k) that you can have,
 given the condition that each rank have different number of candy pieces,
-where the higher the rank, the higher the amount of candy pieces.
+where the higher the place/rank of the winner, the greater the amount of candy pieces he/she recieves.
 
 Samples:
     Input:
@@ -27,50 +27,54 @@ Samples:
     8
     1 2 5
 '''
-PairwiseSummands = namedtuple('PairwiseSummands','bigger smaller')
+def optimal_summands(n):
+    if n == 0:
+        return []
 
-class MaxPrizeGiver:
-    def __init__(self, n):
-        self.n = n
-    
-    def optimal_summands(self):
-        print(f'n: {self.n}')
-        # single_digit_prime_nums = [2, 3, 5, 7]
-        summands = []
+    if n <= 2:
+        return [n]
 
-        # TODO - verify this is returning the correct pairwise sum
-        pair = find_pairwise_summands(self.n)
+    summands = [1, n -1]
+
+    i = 1
+    while (True):
+        subtrahend = i + 1 
+        summand = summands[i] - subtrahend 
+
+        if (summand <= subtrahend):
+            break
         
-        return summands
-    
-    def find_pairwise_summands(self, n):
-        bigger_summand = break_down_to_summand(n)
-        smaller_summand = n - bigger_summand
+        summands.pop()
+        summands.extend([subtrahend, summand])
 
-        return PairwiseSummands(bigger_summand, smaller_summand)
+        i += 1
 
-    def break_down_to_summand(self, n):
-        quotient = divide_by_single_digit_prime_num_or_2(n)
-
-        return quotient + 1
-
-    def divide_by_single_digit_prime_num_or_2(self, n):
-        # single_digit_prime_nums = [2, 3, 5, 7]
-
-        # for prime_number in single_digit_prime_nums:
-        #     if (n % prime_number == 0):
-        #         return n / prime_number
-        
-        # TODO -- might only want to just divide by 2 always
-        return trunc(n/2)
-
+    return summands
 
 if __name__ == '__main__':
     # input = sys.stdin.read()
-    input = 6
+    input = 10
     n = int(input)
-    prize_giver = MaxPrizeGiver(n)
-    summands = prize_giver.optimal_summands()
+    summands = optimal_summands(n)
     print(len(summands))
     for x in summands:
         print(x, end=' ')
+
+
+
+# Pattern
+# 1 => 1 (1)
+
+# 2 => 1 (2)
+
+# 3 => 2 (1, 3)
+
+# 4 => 2 (1, 3)
+
+# 5 => 2 (1, 4)
+
+# 6 => (1, 5) => (1, 2, 3)
+
+# 7 => (1, 6) => (1, 2, 4) => (1, 2, 4)
+
+# 8 => (1, 7) => (1, 2, 5) => (1, 2, 5)
